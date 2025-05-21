5931c628098ab3eb407c00de523b80e932cfa707
LuisAlfonsoPerezRojo <luisalfonsoperezrojo@gmail.com>
2025-05-21 10:20:08 +0200

    Paquetes y atributos encapsulados’

--- BoscoBank/src/banco/Cuenta.java
+++ BoscoBank/src/banco/Cuenta.java
@@ -8,68 +8,36 @@ import java.io.Serializable;
  */
 public class Cuenta implements Serializable{
 
-    private String iban;
-    private String titular;
-    private double saldo;
-    private String dni;
+    public String iban;
+    public String titular;
+    public double saldo;
+    public String dni;
 
     public Cuenta(String iban, String titular, String dni) {
-        this.setIban(iban);
-        this.setTitular(titular);
-        this.setSaldo(0);
-        this.setDni(dni);
+        this.iban = iban;
+        this.titular = titular;
+        this.saldo = 0;
+        this.dni = dni;
     }
 
     public Cuenta() {
-        this.setIban("");
-        this.setTitular("");
-        this.setSaldo(0);
-        this.setDni("");
+        this.iban = "";
+        this.titular = "";
+        this.saldo = 0;
+        this.dni = "";
     }
 
     public void ingresar(double cantidad) {
-        this.setSaldo(this.getSaldo() + cantidad);
+        this.saldo += cantidad;
     }
 
     public void retirar(double cantidad) {
-        this.setSaldo(this.getSaldo() - cantidad);
+        this.saldo -= cantidad;
     }
 
     @Override
     public String toString() {       
-        return String.format("%-24s %-30s %9s %10.2f", this.getIban(), this.getTitular(), this.getDni(), this.getSaldo());
+        return String.format("%-24s %-30s %9s %10.2f", this.iban, this.titular, this.dni, this.saldo);
     }
-
-	public String getDni() {
-		return dni;
-	}
-
-	public void setDni(String dni) {
-		this.dni = dni;
-	}
-
-	public double getSaldo() {
-		return saldo;
-	}
-
-	public void setSaldo(double saldo) {
-		this.saldo = saldo;
-	}
-
-	public String getTitular() {
-		return titular;
-	}
-
-	public void setTitular(String titular) {
-		this.titular = titular;
-	}
-
-	public String getIban() {
-		return iban;
-	}
-
-	public void setIban(String iban) {
-		this.iban = iban;
-	}
 
 }
